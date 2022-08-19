5aba26295c2fc2bc2c6f289263c9cfdbf6b14be3
voma <anna058@naver.com>
2022-08-19 18:09:08 +0900

    scroll custom changed

--- js/common.js
+++ js/common.js
@@ -32,10 +32,6 @@ $(document).ready(function(){
             }
         });
 
-        $(".scroll_container_x").mCustomScrollbar({
-            axis:"x"
-        });
-
         // sub_art_detail
         $(".cnt3.cnt_art").scroll(function(){
             if($(this).scrollTop() > 30){
